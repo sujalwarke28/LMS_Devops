78128c099a097ef96090f584c03c1d8e2fd76966
DevOPS LMS <devops-lms@project.com>
2026-06-13 15:19:41 +0530

    fix: update eks version to 1.30 to resolve AWS deprecation error

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,7 +35,7 @@ variable "availability_zones" {
 variable "eks_cluster_version" {
   description = "EKS Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.30"
 }
 
 variable "node_instance_type" {
